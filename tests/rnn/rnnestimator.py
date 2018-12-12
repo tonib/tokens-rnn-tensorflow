9bbce408cf49f0bb8ac438cee1655aa4b9cbcb06
tonib <toni.bennasar@gmail.com>
2018-12-12 20:34:24 +0100

    rnn: Learn and generalize

--- tests/rnn/rnnestimator.py
+++ tests/rnn/rnnestimator.py
@@ -26,10 +26,22 @@ vocabulary.append('_')
 vocabulary.sort()
 
 def pad_sequence( text_sequence : str ) -> List[str]:
+    """
+    Pads the text_sequence lenght to a minimum length of SEQUENCE_LENGHT, and returns it as a List of characters
+
+    As far I know, Tensorflow RNN estimators don't support variable length sequences, so I'll use the char "_" as padding.
+    If text_sequence has a len(text_sequence) < SEQUENCE_LENGHT, the text will be padded as "_...text_sequence", up to SEQUENCE_LENGHT characters.
+    
+    Args:
+        text_sequence: The text to pad 
+
+    Retunrs:
+        The "text_sequence", padded with "_" characters, as a characters List
+    """
+
     l = len(text_sequence)
     if l < SEQUENCE_LENGHT:
-        # Pad string: "_..._SEQUENCE"
-        print('PADDING!!!')
+        # Pad string: "__...__text_sequence"
         text_sequence = text_sequence.rjust( SEQUENCE_LENGHT , '_')
     
     # Return the text as a characters list
@@ -53,8 +65,8 @@ def prepare_train_sequences_length(seq_length : int):
         outputs.append(sequence_output)
 
 
-# Prepare sequences of a range of lengths
-for sequence_length in range(SEQUENCE_LENGHT, SEQUENCE_LENGHT + 1):
+# Prepare sequences of a range of lengths from 1 to 7 characters
+for sequence_length in range(1, 8):
     prepare_train_sequences_length(sequence_length)
 
 print("N. train sequences: ", len(inputs['character']))
@@ -79,18 +91,21 @@ def input_fn(n_repetitions = 1) -> tf.data.Dataset:
     
     return ds
 
+# The single character sequence 
 character_column = contrib_feature_column.sequence_categorical_column_with_vocabulary_list( 'character' , vocabulary_list = vocabulary )
 indicator_column = feature_column.indicator_column( character_column )
 feature_columns = [ indicator_column ]
 
+# The estimator
 estimator = RNNClassifier(
     sequence_feature_columns=feature_columns,
     num_units=[7], cell_type='lstm', model_dir='./model', 
     n_classes=10, label_vocabulary=vocabulary)
 
+# Traing until all train set is learned
 while test_utils.accuracy(estimator, input_fn) < 1.0:
     print("Training...")
-    estimator.train(input_fn = lambda:input_fn(100))
+    estimator.train(input_fn=input_fn)
 
 def predict( text : str ):
     """
@@ -104,16 +119,21 @@ def predict( text : str ):
     print("-----")
     print("Input sequence: " , text )
     for r in result:
-        print(r)
-        print('Output:', r['class_ids'])
+        #print("Prediction: " , r)
+        print('Class output: ', r['class_ids'])
     print("-----")
 
-
+# Some predictions in the train set (memory)
 predict( '0123456' )
 predict( '1234567' )
 predict( '2345678' )
 predict( '3456789' )
 predict( '4567890' )
-predict( '3' ) # This fails
-predict( '5678' ) # This fails
-predict( '2345678901' ) # This fails
+predict( '3' )
+predict( '5678' )
+
+# Some predictions out the train set (generalization)
+predict( '2345678901' )
+predict( '6789012345678' )
+predict( '9012345678901234567890123456789012' )
+predict( '0123456789012345678901234567890123456789' )
