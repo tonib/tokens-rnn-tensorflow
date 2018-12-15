475c49a951132b141748ca41db98cb41a4709d87
tonib <toni.bennasar@gmail.com>
2018-12-15 11:36:20 +0100

    Fixed bug with quixote dataset, added entire quixote text

--- tests/rnn-quixote/rnnquixote.py
+++ tests/rnn-quixote/rnnquixote.py
@@ -4,11 +4,13 @@ from tensorflow.contrib.estimator import RNNClassifier
 import tensorflow.contrib.feature_column as contrib_feature_column
 import argparse
 
+#tf.enable_eager_execution()
+
 # Get commnad line arguments
 parser = argparse.ArgumentParser(
     formatter_class=argparse.ArgumentDefaultsHelpFormatter)
 parser.add_argument('--op', type=str, default='train',
-                    help='"train" to train, "generate" to generate text')
+                    help='"train" to train, "generate" to generate text, "debugds" to debug dataset')
 args = parser.parse_args()
 print( args.op )
 
@@ -16,6 +18,10 @@ print( args.op )
 with open( 'quixote.txt' , 'r')  as file:
     text = file.read()
 #print(text)
+
+# Right now all if I try to run this will all text, it eats all my computer memory and it hags. Avoid it:
+if len(text) > 1000:
+    text = text[:1000]
 
 # Sequence length that will be feeded to the network
 SEQUENCE_LENGHT = 200
@@ -35,7 +41,7 @@ outputs =  []
 for i in range(0, len(text) - SEQUENCE_LENGHT):
     sequence = text[i : i + SEQUENCE_LENGHT]
     sequence_output = text[i + SEQUENCE_LENGHT : i + SEQUENCE_LENGHT+1]
-    inputs['character'].append( sequence )
+    inputs['character'].append( list(sequence) )
     outputs.append(sequence_output)
     #print("Sequence: " , sequence , ", Output: " , sequence_output)
 
@@ -49,8 +55,11 @@ feature_columns = [ indicator_column ]
 # The estimator
 estimator = RNNClassifier(
     sequence_feature_columns=feature_columns,
-    num_units=[200], cell_type='lstm', model_dir='./model', 
-    n_classes=len(vocabulary), label_vocabulary=vocabulary)
+    num_units=[200], cell_type='gru', 
+    optimizer=tf.train.AdamOptimizer,
+    model_dir='./model', 
+    n_classes=len(vocabulary), 
+    label_vocabulary=vocabulary)
 
 def input_fn(n_repetitions = 1) -> tf.data.Dataset:
     """
@@ -68,7 +77,7 @@ def input_fn(n_repetitions = 1) -> tf.data.Dataset:
         ds = ds.repeat(n_repetitions)
 
     ds = ds.shuffle( 1000 )
-    ds = ds.batch(8)
+    ds = ds.batch(64)
     
     return ds
 
@@ -95,16 +104,51 @@ def predict_char( text : str ) -> str:
     print("Input sequence: '" , text , "'")
     for r in result:
         print('Prediction: ' , r)
+
         c = r['classes'][0].decode( 'utf-8' )
         print( 'Class output: "', c , '"')
+
+        # Instead returning the most probable character, get a random sample (see https://www.tensorflow.org/tutorials/sequences/text_generation):
+        g_1 = tf.Graph()
+        with g_1.as_default():
+            with tf.Session():
+                logits = tf.reshape( r['logits'] , ( 1 , -1 ) )
+                idx = tf.multinomial( logits=logits , num_samples=1)
+                idx = tf.squeeze(idx,axis=-1).eval()
+                print("Multinomial idx: " , idx, ", Character: " , vocabulary[idx[0]] )
+                c = vocabulary[idx[0]]
+
         return c
     print("-----")
+    return '?'
 
 def predict_text( text : str ) -> str:
     result = text
-    for _ in range(10):
+    for _ in range(50):
         result += predict_char( result )
     print(result)
+
+def debug_ds(ds, print_ds=True):
+    if print_ds:
+        print(ds)
+        print()
+
+    it = ds.make_one_shot_iterator()
+    n_elements = 0
+    while True:
+        try:
+            v = it.get_next()
+        except:
+            break
+        
+        n_elements += 1
+        if print_ds:
+            print(v)
+            # print(v[0])
+            # print(v[1])
+            print()
+
+    print("N.elements: ", n_elements)
 
 if args.op == 'train':
     # Training loop
@@ -112,7 +156,11 @@ if args.op == 'train':
     while True:
         estimator.train(input_fn=input_fn)
         accuracy(estimator, input_fn)
+elif args.op == 'debugds':
+    debug_ds(input_fn() , True)
 else:
-    predict_text( 'En un lugar de la Mancha' )
+    start_text = text[:SEQUENCE_LENGHT]
+    print("start_text len:", len(start_text))
+    predict_text( start_text )
     #predict_text( 'abc' )
 
